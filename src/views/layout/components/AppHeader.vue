30b5d96fe94576306050a8ecbd4bcaa650d0a5f4
ThisIsNotJacob <thisisnotjacob@163.com>
2019-06-26 21:15:59 +0800

    Layout组件-Header布局

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        <p>AppHeader Component</p>
-    </div>
+  <el-row>
+    <el-col :span="14">
+      <div class="grid-content bg-purple">头条后台管理系统</div>
+    </el-col>
+    <el-col :span="10" :offset="2">
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link">
+          <img width="30" src="http://toutiao.meiduo.site/Fkj6tQi3xJwVXi1u2swCElotfdCi">
+          17660471520<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>账户设置</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -14,5 +28,26 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.el-header {
+  line-height: 60px;
+}
+.el-col {
+  height: 60px;
+  line-height: 60px;
+}
+.el-dropdown {
+  height: 60px;
+  line-height: 60px;
+  float: right;
+  cursor: pointer;
 
+}
+img {
+  border-radius: 50%;
+  vertical-align: middle;
+}
+.el-col-10 {
+  margin: 0;
+  padding: 0;
+}
 </style>
