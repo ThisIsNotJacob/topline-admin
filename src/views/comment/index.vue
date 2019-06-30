d7b5caeca1368b6e2737df6f9600ae2b23167955
ThisIsNotJacob <thisisnotjacob@163.com>
2019-06-30 11:50:56 +0800

    评论管理-修改文章评论状态-处理开关禁用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,6 +10,7 @@
       <el-table-column label="允许评论">
         <template slot-scope="scope">
           <el-switch
+            :disabled="scope.row.changeStatusLoading"
             v-model="scope.row.comment_status"
             active-color="#13ce66"
             inactive-color="#ff4949"
@@ -41,10 +42,14 @@ export default {
           response_type: 'comment'
         }
       }).then(data => {
+        data.results.forEach(item => {
+          item.changeStatusLoading = false
+        })
         this.articles = data.results
       })
     },
     handleChangeStatus(item) {
+      item.changeStatusLoading = true
       this.$http({
         method: 'PUT',
         url: '/comments/status',
@@ -59,7 +64,10 @@ export default {
           type: 'success',
           message: `评论状态已变更为${item.comment_status ? '允许' : '关闭'}`
         })
+        item.changeStatusLoading = false
       }).catch(err => {
+        item.changeStatusLoading = false
+        item.comment_status = !item.comment_status
         console.log(err)
         this.message.error('更改状态失败')
       })
